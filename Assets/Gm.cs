99630821feee02df083a9924af725466be174142
ehammo <ehammo@cin.ufpe.br>
2016-12-10 22:40:06 -0300

    Added a game manager to pass the parameters of the char from a scene to another

---
+++ Assets/Gm.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Gm : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float maxMana = 100;
+    public int maxDamage = 25;
+    public float maxArmor = 0;
+
+    public float currentHealth = 100;
+    public float currentMana = 50;
+    public int currentDamage = 10;
+    public float currentArmor = 0;
+
+
+    void Awake()
+    {
+
+        // Do not destroy this game object:
+
+        DontDestroyOnLoad(this);
+
+    }
+}
